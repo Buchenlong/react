076babb802a04e37726fb2d65e3aa2d0d5bf639d
Buchenlong <1281477524@qq.com>
2020-10-04 20:34:37 +0800

    react

---
+++ build/precache-manifest.f8f1383b73c591f9d34575294ae50ce9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "98afcb6a6e4fca623581f0cda2a03778",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b766cac13db4c6841089",
+    "url": "/static/js/2.fdf38fef.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.fdf38fef.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b62c31d3452ce51616ff",
+    "url": "/static/js/main.49660cae.chunk.js"
+  },
+  {
+    "revision": "e2702865a21868f2ebe6",
+    "url": "/static/js/runtime-main.7918a0be.js"
+  }
+]);
